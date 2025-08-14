419a78dc234e64cdfcfc7853307907721f7f3f0e
DelaRosa <delarosamonteiro@gmail.com>
2025-08-14 14:28:26 +0200

    refactor: Update relatorySchema to use coercion and validation for title and date fields

--- src/views/dashboards/relatory/Schemas.tsx
+++ src/views/dashboards/relatory/Schemas.tsx
@@ -1,12 +1,20 @@
 import { z } from 'zod'
 
-export const cardSchema = z.object({
-  title: z.string().min(1, 'O título é obrigatório'),
-  date: z.string().min(1, 'A data é obrigatória'),
-  shortDescription: z.string().min(1, 'A descrição curta é obrigatória'),
+export const relatorySchema = z.object({
+  title: z.coerce.string().trim().min(1, 'Title is required'),
+  date: z.string().refine(
+    val => {
+      if (!val) return true
+
+      return !isNaN(Date.parse(val))
+    },
+    {
+      message: 'Invalid date format'
+    }
+  ),
+  shortDescription: z.string().optional(),
   longDescription: z.string().optional(),
   image: z.string().optional()
 })
 
-// Se quiser, pode exportar o tipo TypeScript também
-export type CardData = z.infer<typeof cardSchema>
+export type RelatoryData = z.infer<typeof relatorySchema>
